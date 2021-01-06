b766a4e437aaeb55729f0f51ddd006177963835f
ssongtheharrykane <61009064+ssongtheharrykane@users.noreply.github.com>
2021-01-06 20:23:20 +0900

    Create programmers_lv3_네트워크.py
    210106-5

---
+++ graph/programmers_lv3_네트워크.py
@@ -0,0 +1,27 @@
+# Undirected Graph에서 Connected Components 찾기
+# DFS / BFS 수행 횟수 = Connected Component의 갯수
+
+def solution(n, computers):
+    answer = 0
+    visited = [False]*n
+    stack = []
+    
+    # Start DFS from each node(computer), if available
+    for i in range(n):
+        if visited[i]==False:
+            stack.append(i)
+
+            # Start DFS(방문할 노드가 스택에 남아있는 경우)
+            while stack: # len(stack) 으로 비어있는지 확인하는 것보다 훨씬 빠름
+                start = stack.pop()
+                visited[start] = True
+                adj_coms = computers[start]
+                for j in range(n):
+                    if start != j:
+                        if adj_coms[j]==1:
+                            if not visited[j]:
+                                stack.append(j)
+                                
+            # DFS가 끝나면 = 하나의 Connected Component의 모든 Node에 대한 Search가 완료된 것 = 한 네트워크의 모든 컴퓨터를 다 방문한 것 => 네트워크 갯수 +1
+            answer+=1
+    return answer
